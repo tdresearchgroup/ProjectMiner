fd4bbfc243782ae3b2b5f3f459f77293223bd4b3
Derek Reimanis <derek.reimanis@gmail.com>
2020-03-06 13:50:26 -0700

    outputting, should be nearly set up for 60 requests/hour run

--- src/main/java/ProjectHandler.java
+++ src/main/java/ProjectHandler.java
@@ -1,7 +1,10 @@
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Table;
 import com.google.common.collect.TreeBasedTable;
 import com.sun.deploy.util.ArrayUtil;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -16,19 +19,18 @@ public class ProjectHandler {
     private List<Integer> tickets;
     private RESTClient client;
 
-    /**
-     * tracks Ticket_number, keyword, and keyword_count for a single project.
-     */
-    private Table<Integer, String, Integer> keywordFrequency;
+    private List<Pair<Integer, Integer>> ticketFrequency;
+
 
     public ProjectHandler(){
         client = new RESTClient();
-        keywordFrequency = TreeBasedTable.create();
+        ticketFrequency = new ArrayList<>();
     }
 
     public void operate(){
         importKeywords();
         fillKeywords();
+        output();
     }
 
     private void importKeywords(){
@@ -48,15 +50,34 @@ public class ProjectHandler {
     }
 
     private void fillKeywords(){
+
         for (Integer ticketNum : tickets){
             //build url
             String url = URIBuilder.getTicketURL(ticketNum);
-            process(ticketNum, url);;
+            try {
+                process(ticketNum, url);
+            }catch (Exception e){
+                output();
+                e.printStackTrace();
+                System.out.println("At the limit, killing the program and outputting.");
+                System.exit(0);
+            }
         }
     }
 
-    private void process(int ticketNum, String url){
+    /***
+     *
+     * @param ticketNum
+     * @param url
+     * @throws Exception THIS IS IMPORTANT BECAUSE THE EXCEPTION WILL BE THROWN IF WE HIT THE #REQUESTS/HOUR LIMIT
+     */
+    private void process(int ticketNum, String url) throws Exception{
         JSONObject jsonObject = getJSONFromSend(url);
+        //I need to make sure this is closed first.
+        String state = (String)jsonObject.get("state");
+        if (state == null || !state.equals("closed")){
+            return;
+        }
         String body = (String)jsonObject.get("body");
         try {
             //sleep so github doesn't think im ddosing them, though realistically, this isn't necessary
@@ -71,16 +92,15 @@ public class ProjectHandler {
         //break up body
         String[] individualText = body.split(" ");
         //loop through keywords on separate document, search body for the keyword. if match, call keyword.put(ticketNum, keyword, count++)
+        int keywordCount = 0;
         for (String keyword : keywords){
             if (ArrayUtils.contains(individualText, keyword)){
                 System.out.println("Got a hit, matched ticket #: " + ticketNum + " with body: " + body + " to keyword: " + keyword);
-                int count = keywordFrequency.get(ticketNum, keyword);
-                if (count == 0){
-                    //hasn't been found yet... or its null idk
-                }
-                keywordFrequency.put(ticketNum, keyword, count++);
+                keywordCount++;
             }
         }
+        //fill data structure
+        ticketFrequency.add(new ImmutablePair<>(ticketNum, keywordCount));
     }
 
 
@@ -102,8 +122,47 @@ public class ProjectHandler {
 
     private void fillTickets(int maxSize){
         tickets = new ArrayList<>();
-        for (int i = 1; i <= maxSize; i++){
+        //
+        for (int i = maxSize-59; i <= maxSize; i++){
             tickets.add(i);
+        }
+        //for when we don't have to worry about 60 most recent tickets.
+//        for (int i = 1; i <= maxSize; i++){
+//            tickets.add(i);
+//        }
+    }
+
+    private void output(){
+        try {
+            //output only non-zero keyword count fields
+            FileWriter nonZero = new FileWriter(new File("nonZero_output.csv"));
+            String delim = ", ";
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Project_ID, Ticket_num, num_matches\n");
+            for (Pair<Integer, Integer> keywordPair : ticketFrequency){
+                if (keywordPair.getRight() > 0){
+                    stringBuilder.append(Main.github_repo_name + delim);
+                    stringBuilder.append(keywordPair.getLeft() + delim);
+                    stringBuilder.append(keywordPair.getRight() + "\n");
+                }
+            }
+            nonZero.write(stringBuilder.toString());
+            nonZero.close();
+
+            FileWriter zero = new FileWriter(new File("zero_output.csv"));
+            stringBuilder = new StringBuilder();
+            stringBuilder.append("Project_ID, Ticket_num, num_matches\n");
+            for (Pair<Integer, Integer> keywordPair : ticketFrequency){
+                stringBuilder.append(Main.github_repo_name + delim);
+                stringBuilder.append(keywordPair.getLeft() + delim);
+                stringBuilder.append(keywordPair.getRight() + "\n");
+            }
+            zero.write(stringBuilder.toString());
+            zero.close();
+
+            //output all fields
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
